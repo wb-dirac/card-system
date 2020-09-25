febe7f422992959efcffca4015086d2921451733
WangBo <wangbo@shoplex.com>
2020-09-25 19:44:14 +0800

    簡繁轉換

--- public/shop_theme/classic/common.js
+++ public/shop_theme/classic/common.js
@@ -6,20 +6,20 @@ var shopType = 'shop';
 var contactType = 'any';
 var contactTypeText = {
     email: {
-        title: '联系邮箱',
-        placeholder: '请输入邮箱，用于查询订单'
+        title: '聯系郵箱',
+        placeholder: '請輸入郵箱，用於查詢訂單'
     },
     mobile: {
-        title: '联系手机号',
-        placeholder: '请输入手机号，用于查询订单'
+        title: '聯系手機號',
+        placeholder: '請輸入手機號，用於查詢訂單'
     },
     qq: {
-        title: '联系QQ',
-        placeholder: '请输入QQ号码，用于查询订单'
+        title: '聯系QQ',
+        placeholder: '請輸入QQ號碼，用於查詢訂單'
     },
     any: {
-        title: '联系方式',
-        placeholder: '可以输入QQ、邮箱、手机号等等，用于查询订单'
+        title: '聯系方式',
+        placeholder: '可以輸入QQ、郵箱、手機號等等，用於查詢訂單'
     }
 };
 var contactExt = [];
@@ -35,8 +35,8 @@ $(function () {
         },
         error: function (e, _, type) {
             msg({
-                title: '请求失败',
-                content: e.responseText ? JSON.parse(e.responseText).message : '网络连接错误: ' + type,
+                title: '請求失敗',
+                content: e.responseText ? JSON.parse(e.responseText).message : '網絡連接錯誤: ' + type,
                 type: 'error'
             });
         }
@@ -46,7 +46,7 @@ $(function () {
 function randomString(len) {
     len = len || 16;
     var $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';
-    /** **默认去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1****/
+    /** **默認去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1****/
     var maxPos = $chars.length;
     var pwd = '';
     for (var i = 0; i < len; i++) {
@@ -138,10 +138,10 @@ function clearProductInfo() {
     currentProduct = null;
     currentCouponInfo = null;
     $('#price').html(' - ');
-    // 批发优惠
+    // 批發優惠
     $('#discount-btn').hide();
     $('#discount-tip').html('');
-    // 库存
+    // 庫存
     $('#invent').html('');
     $('#description').html('');
 
@@ -156,7 +156,7 @@ function getProducts(category_id) {
 
     selectProduct(-1);
     if (config.theme.list_type === 'dropdown') {
-        $('#products > option:first-child').text('加载中...');
+        $('#products > option:first-child').text('加載中...');
     }
 
     for (var i = 0; i < config.categories.length; i++) {
@@ -183,7 +183,7 @@ function getProducts(category_id) {
         if (config.theme.list_type === 'button') {
             $('#products').html(children);
         } else {
-            $('#products').html('<option value="-1">请选择商品</option>').append(children);
+            $('#products').html('<option value="-1">請選擇商品</option>').append(children);
         }
         if (products.length === 1) {
             selectProduct(products[0].id);
@@ -203,13 +203,13 @@ function getProducts(category_id) {
     var next = function () {
         $.post('/api/shop/product', queryData).success(function (res) {
             if (currentCategory.id !== +category_id) {
-                // 如果在请求过程中已经选择了另一个分类, 则返回
+                // 如果在請求過程中已經選擇了另壹個分類, 則返回
                 return;
             }
             if (!res.data.length) {
                 res.data = [{
                     id: -2,
-                    name: '此分类下没有商品'
+                    name: '此分類下沒有商品'
                 }]
             }
             currentCategory.password = queryData.password;
@@ -220,21 +220,21 @@ function getProducts(category_id) {
             if (config.theme.list_type === 'button') {
                 $('#products').html('');
             } else {
-                $('#products').html('<option value="-1">请选择商品</option>');
+                $('#products').html('<option value="-1">請選擇商品</option>');
             }
             selectCategory(-1);
         });
     };
 
     if (currentCategory.password_open) {
-        passwordDialog('请输入分类密码', function (password) {
+        passwordDialog('請輸入分類密碼', function (password) {
             if (!password || !password.length) {
-                showToast('warn', '请输入分类密码');
+                showToast('warn', '請輸入分類密碼');
                 selectCategory(-1);
                 if (config.theme.list_type === 'button') {
                     $('#products').html('');
                 } else {
-                    $('#products > option:first-child').text('请选择商品');
+                    $('#products > option:first-child').text('請選擇商品');
                 }
                 return;
             }
@@ -246,14 +246,14 @@ function getProducts(category_id) {
     }
 }
 
-// 选择产品时, 显示详细信息
+// 選擇產品時, 顯示詳細信息
 function showProductInfo(product) {
     clearProductInfo();
 
     if (product.id < 0) return;
 
     function renderInfoToHtml() {
-        // 联系方式类型限制以及自定义字段
+        // 聯系方式類型限制以及自定義字段
         if (product.fields) {
             try {
                 if (typeof product.fields === 'string' && product.fields[0] === '{') {
@@ -275,7 +275,7 @@ function showProductInfo(product) {
         $('#contact-box input').attr('placeholder', contactTypeText[contactType].placeholder);
 
         $('#price').text((product.price / 100).toFixed(2));
-        $('#invent').html('库存: ' + product.count2);
+        $('#invent').html('庫存: ' + product.count2);
         $('#description').html(renderQuill(product.description)).show();
 
         if (product.price_whole && typeof product.price_whole === 'string') {
@@ -285,10 +285,10 @@ function showProductInfo(product) {
         if (product.price_whole && product.price_whole.length) {
             var msg = '';
             product.price_whole.forEach(function (e) {
-                msg += '满' + e[0] + '件，单价<b>' + (e[1] / 100).toFixed(2) + '</b>元<br>';
+                msg += '滿' + e[0] + '件，單價<b>' + (e[1] / 100).toFixed(2) + '</b>元<br>';
             });
             $('#discount-btn').fadeIn();
-            $('#discount-tip').html('<p>优惠<br>' + msg + '<p>')
+            $('#discount-tip').html('<p>優惠<br>' + msg + '<p>')
         }
 
         if (product.support_coupon) {
@@ -302,9 +302,9 @@ function showProductInfo(product) {
 
     if (product.password_open && !product.password) {
         var currentChoose = product;
-        passwordDialog('请输入商品密码', function (password) {
+        passwordDialog('請輸入商品密碼', function (password) {
             if (!password || !password.length) {
-                showToast('warn', '请输入商品密码');
+                showToast('warn', '請輸入商品密碼');
                 selectProduct(-1);
                 return;
             }
@@ -336,7 +336,7 @@ function getCouponInfo() {
         currentCouponInfo = res.data;
         calcTotalPrice();
     }).error(function () {
-        showToast('warn', '优惠券信息无效')
+        showToast('warn', '優惠券信息無效')
     });
 }
 
@@ -376,7 +376,7 @@ function calcTotalPrice() {
             off = currentCouponInfo.discount_val + '%'
         }
         price_pay -= discount;
-        $('#coupon-tip').text('立减' + off + ', 已优惠:' + (discount / 100).toFixed(2));
+        $('#coupon-tip').text('立減' + off + ', 已優惠:' + (discount / 100).toFixed(2));
     }
     var fee_tip = '';
     if (1 === +window.config.shop.fee_type) {
@@ -388,7 +388,7 @@ function calcTotalPrice() {
         }
         if (fee > 0) {
             price_pay += fee;
-            fee_tip = ' <span style="font-size: 8px">(手续费' + (fee / 100).toFixed(2) + ') </span>';
+            fee_tip = ' <span style="font-size: 8px">(手續費' + (fee / 100).toFixed(2) + ') </span>';
         }
     }
 
@@ -405,14 +405,14 @@ function assertTradeAmount() {
     var count = $('#quantity').val();
     if (count < currentProduct.buy_min || currentProduct.buy_max < count) {
         if (currentProduct.buy_min === currentProduct.buy_max) {
-            var tip = '此商品只能购买&nbsp;' + currentProduct.buy_min + '</b>&nbsp;件'
+            var tip = '此商品只能購買&nbsp;' + currentProduct.buy_min + '</b>&nbsp;件'
         } else {
-            tip = '最少购买&nbsp;<b>' + currentProduct.buy_min + '</b>&nbsp;件<br>最多购买&nbsp;<b>' + currentProduct.buy_max + '</b>&nbsp;件';
+            tip = '最少購買&nbsp;<b>' + currentProduct.buy_min + '</b>&nbsp;件<br>最多購買&nbsp;<b>' + currentProduct.buy_max + '</b>&nbsp;件';
         }
         msg({
             title: '提示',
-            content: '购买数目限制<br>' + tip,
-            btn: ['关闭'],
+            content: '購買數目限制<br>' + tip,
+            btn: ['關閉'],
             then: function () {
                 $('#quantity').val(currentProduct.buy_min).focus()
             }
@@ -422,16 +422,16 @@ function assertTradeAmount() {
     if (currentProduct.count === 0) {
         msg({
             title: '提示',
-            content: '当前商品库存不足',
-            btn: ['关闭']
+            content: '當前商品庫存不足',
+            btn: ['關閉']
         });
         return false;
     }
     if (+currentProduct.count && +currentProduct.count < count) {
         msg({
             title: '提示',
-            content: '购买数目不能超出商品库存<br>当前商品库存&nbsp;<b>' + currentProduct.count + '</b>&nbsp;件',
-            btn: ['关闭'],
+            content: '購買數目不能超出商品庫存<br>當前商品庫存&nbsp;<b>' + currentProduct.count + '</b>&nbsp;件',
+            btn: ['關閉'],
             then: function () {
                 $('[name=quantity]').focus();
             }
@@ -548,20 +548,20 @@ function order(type) {
     }
     window.open(orderUrl, '_blank');
 
-    showOrderTip('请在弹出的窗口完成付款<br><span style="font-size:13px">如果没有弹出窗口或付款失败，您也可以返回重新发起付款</span>', function () {
+    showOrderTip('請在彈出的窗口完成付款<br><span style="font-size:13px">如果沒有彈出窗口或付款失敗，您也可以返回重新發起付款</span>', function () {
         window.open('/s#/record?tab=cookie', '_blank')
     });
 }
 
 function checkOrder() {
     if (!currentCategory) {
-        showToast('error', '请选择商品分类');
+        showToast('error', '請選擇商品分類');
         $('#categories').focus();
         return false; // 阻止冒泡
     }
 
     if (!currentProduct) {
-        showToast('error', '请选择商品');
+        showToast('error', '請選擇商品');
         $('#products').focus();
         return false;
     }
@@ -583,24 +583,24 @@ function checkOrder() {
     };
 
     if (contactType === 'any') {
-        if (!contact) return showError('请填写您的联系信息，如QQ、邮箱、手机号等等，用于查询订单');
-        if (contact.length < 6) return showError('联系方式长度至少为6位');
+        if (!contact) return showError('請填寫您的聯系信息，如QQ、郵箱、手機號等等，用於查詢訂單');
+        if (contact.length < 6) return showError('聯系方式長度至少為6位');
     } else if (contactType === 'email') {
-        if (!contact) return showError('请填写您的邮箱，用于查询订单');
-        if (!validateEmail(contact)) return showError('输入的邮箱格式不正确');
+        if (!contact) return showError('請填寫您的郵箱，用於查詢訂單');
+        if (!validateEmail(contact)) return showError('輸入的郵箱格式不正確');
     } else if (contactType === 'mobile') {
-        if (!contact) return showError('请填写您的手机号码，用于查询订单');
-        if (!validateMobile(contact)) return showError('输入的手机号格式不正确');
+        if (!contact) return showError('請填寫您的手機號碼，用於查詢訂單');
+        if (!validateMobile(contact)) return showError('輸入的手機號格式不正確');
     } else if (contactType === 'qq') {
-        if (!contact) return showError('请填写您的QQ号码，用于查询订单');
+        if (!contact) return showError('請填寫您的QQ號碼，用於查詢訂單');
         if (contact.length < 5 || contact.length > 11 || !validatNumbers(this.contact))
-            return showError('输入的QQ号码格式不正确');
+            return showError('輸入的QQ號碼格式不正確');
     }
 
     if (contactExtValues.length) {
         for (var i = 0; i < contactExtValues.length; i++) {
             if (!contactExtValues[i]) {
-                return showError('请填写 ' + contactExt[i])
+                return showError('請填寫 ' + contactExt[i])
             }
         }
     }
@@ -609,10 +609,10 @@ function checkOrder() {
     if (sendSms && sendSms.checked) {
         var smsTo = $('#sms_to').val();
         if (!smsTo) {
-            return showError('请填写需要接受订单信息的手机号码', '#sms_to');
+            return showError('請填寫需要接受訂單信息的手機號碼', '#sms_to');
         }
         if (!validateMobile(smsTo)) {
-            return showError('输入的手机号格式不正确', '#sms_to');
+            return showError('輸入的手機號格式不正確', '#sms_to');
         }
     }
 
@@ -620,10 +620,10 @@ function checkOrder() {
     if (sendMail && sendMail.checked) {
         var mailTo = $('#mail_to').val();
         if (!mailTo) {
-            return showError('请填写需要接受订单信息的邮箱', '#mail_to');
+            return showError('請填寫需要接受訂單信息的郵箱', '#mail_to');
         }
         if (!validateEmail(mailTo)) {
-            return showError('输入的邮箱格式不正确', '#mail_to');
+            return showError('輸入的郵箱格式不正確', '#mail_to');
         }
     }
 
@@ -638,7 +638,7 @@ $(function () {
         var ann_pop = renderQuill(config.shop.ann_pop, true);
         if (ann_pop) {
             swal({
-                title: '店铺公告',
+                title: '店鋪公告',
                 html: '<div class="ql-editor quill-html">' + ann_pop + '</div>'
             });
         }
@@ -701,7 +701,7 @@ $(function () {
 
         if (config.categories.length === 1) {
             categoriesElm.val(config.categories[0].id);
-            getProducts(config.categories[0].id);  // 一个分类时, 商品预加载
+            getProducts(config.categories[0].id);  // 壹個分類時, 商品預加載
             if (config.categories[0].password_open === false) {
                 categoriesElm.prop('disabled', true);
             }
@@ -741,11 +741,11 @@ $(function () {
             initGeetest({
                 gt: gt_config.gt,
                 challenge: gt_config.challenge,
-                offline: !gt_config.success, // 表示用户后台检测极验服务器是否宕机
-                product: 'bind', // 这里注意, 2.0请改成 popup
+                offline: !gt_config.success, // 表示用戶後臺檢測極驗服務器是否宕機
+                product: 'bind', // 這裏註意, 2.0請改成 popup
                 width: '300px',
                 https: true
-                // 更多配置参数说明请参见：http://docs.geetest.com/install/client/web-front/
+                // 更多配置參數說明請參見：http://docs.geetest.com/install/client/web-front/
             }, function (captchaObj) {
                 captchaObj.onReady(function () {
                     console.log('geetest: onReady')
@@ -754,8 +754,8 @@ $(function () {
                     console.log('geetest: onError');
                     console.error(e);
                     msg({
-                        title: '出错了',
-                        content: '下单验证码加载失败, 请刷新重试',
+                        title: '出錯了',
+                        content: '下單驗證碼加載失敗, 請刷新重試',
                         type: 'error',
                         then: function () {
                             location.reload();
@@ -765,7 +765,7 @@ $(function () {
                 captchaObj.onSuccess(function () {
                     var result = captchaObj.getValidate();
                     if (!result) {
-                        return alert('请完成验证');
+                        return alert('請完成驗證');
                     }
                     codeValidate = {
                         'captcha.a': gt_config.key,
@@ -774,8 +774,8 @@ $(function () {
                         'captcha.d': result.geetest_seccode
                     };
                     msg({
-                        title: '验证完成',
-                        content: '验证成功，请点击按钮跳转支付页面',
+                        title: '驗證完成',
+                        content: '驗證成功，請點擊按鈕跳轉支付頁面',
                         btn: ['去支付'],
                         then: function () {
                             order();
@@ -797,7 +797,7 @@ $(function () {
                 // captchaObj.appendTo('#gt-btn-verify');
                 if (captchaObj.bindOn) {
                     captchaObj.bindOn('#gt-btn-verify');
-                    // 3.0 没有 bindOn 接口
+                    // 3.0 沒有 bindOn 接口
                 }
             });
         }
